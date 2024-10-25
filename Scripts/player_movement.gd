22b0baaffa5f44051a7db81d87330e2b4663c918
Paul Mackiewicz <101292605+Skytra05@users.noreply.github.com>
2024-10-25 13:55:45 -0500

    Line Pointing Update

--- Scripts/player_movement.gd
+++ Scripts/player_movement.gd
@@ -3,7 +3,12 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var ghostOut = false
+var mousePOS
+var playerPOS
 
+var lineAngle
+var lineLength = 100
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -23,3 +28,15 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	if Input.is_action_just_pressed("castout"): #E is the current button for this
+		mousePOS = get_global_mouse_position()
+		playerPOS = $CollisionShape2D.global_position
+		lineAngle = mousePOS.angle_to_point(playerPOS)
+		
+		#Probably going to be mainly used for debugging but a good starting point
+		$RayCast2D/Line2D.set_point_position(1, Vector2(-lineLength * cos(lineAngle),-lineLength * sin(lineAngle))) #(sin(lineAngle)/cos(lineAngle)))
+		#print($RayCast2D/Line2D.get_point_position(1))
+		#print($RayCast2D/Line2D.get_point_position(0))
+	
+#func _unhandled_key_input(Input.is_action_pressed("castout")):
